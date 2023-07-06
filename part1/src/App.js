383cab411dced39dc381eed542fec554c664fbc9
“Brian <brianshanley242@gmail.com>
2023-07-06 12:34:13 -0400

    Exercises 1.12 - 1.14 completed

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,108 +1,61 @@
 import { useState } from 'react'
 
 const Button = (props) => {
+  
   return (
-    <button onClick={props.handleClick}>
-    {props.text}
-  </button>
+    <button onClick={props.handleClick}>{props.text}</button>
   )
 }
-
-const StatisticLine = (props) => {
-  return (
-    <tr>
-      <td>{props.text}</td> 
-      <td>{props.value}</td>
-    </tr>
-  )
-}
-
-
-const Statistics = (props) => {
-  if (props.allValues > 0){
-    return (
-      <>
-      <table>
-        <tbody>
-          <StatisticLine value={props.goodValue} text={props.goodText} />
-          <StatisticLine value={props.neutralValue} text={props.neutralText} />
-          <StatisticLine value={props.badValue} text={props.badText} />
-          <StatisticLine value={props.calculateTotal} text={props.totalText} />
-          <StatisticLine value={props.calculateAverage} text={props.averageText} />
-          <StatisticLine value={props.calculatePositive} text={props.positiveText} />
-        </tbody>
-      </table>
-      </>
-    )
-  } else {
-    return (
-      <div>No Feedback Given</div>
-    )
-  }
-}
-
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const goodFeedback = () => {
-    console.log("goodfeedback")
-    setGood(good + 1)
-    console.log(good, "good feedback")
-  }
-
-  const neutralFeedback = () => {
-    console.log("neutralfeedback")
-    setNeutral(neutral + 1)
-    console.log(neutral, "neutral feedback")
-  }
-
-  const badFeedback = () => {
-    console.log("badfeedback")
-    setBad(bad + 1)
-    console.log(bad, "bad feedback")
-  }
-
-  // Calculate the average feedback
-  const calculateAverage = () => {
-    const totalFeedbacks = good + neutral + bad;
-    const sumFeedbacks = good - bad;  // neutral feedback is 0 so no need to include it
-
-    // Round to two decimals
-    const roundedAverage = Math.round((sumFeedbacks / totalFeedbacks)*100)/100
-
-    return totalFeedbacks ? roundedAverage : 0;
-  }
-
-  // Calculate the percentage of positive feedback
-const calculatePositive = () => {
-  const totalFeedbacks = good + neutral + bad;
-  const percentagePositive = (good / totalFeedbacks) * 100;
   
-  // Use Math.round() to round to the nearest whole number
-  const roundedPercentage = Math.round(percentagePositive);
-  
-  return totalFeedbacks ? `${roundedPercentage}%` : '0%';
-}
 
-// Calculate the feedback total
-const calculateTotal = () => {
-  return good + neutral + bad;
-}
+  const [points, setPoints] = useState([0,0,0,0,0,0,0,0])
+   
+  const [selected, setSelected] = useState(0)
 
+  const handleClick = () => {
+    const random = Math.floor(Math.random() * anecdotes.length)
+    console.log(random)
+    setSelected(random)
+  }
 
+  const increasePoints = () =>{
+    // make copy of points array
+    const copy = [...points]
+    // update the proper one by one
+    copy[selected] += 1
+    // set new state of points array
+    setPoints(copy)
+  }
+
+  // Get the highest-vote anecdote
+  const highestVoteIndex = points.reduce((maxIndex, currentNumber, currentIndex, array) => {
+    return currentNumber > array[maxIndex] ? currentIndex : maxIndex;
+}, 0);
+
+console.log(highestVoteIndex)
 
   return (
     <div>
-      <h1>Give feedback</h1>
-      <Button handleClick={goodFeedback} text="Good Feeback!"/>
-      <Button handleClick={neutralFeedback} text="Neutral Feedback!"/>
-      <Button handleClick={badFeedback} text="Bad Feedback!"/>
-      <h2>Statistics</h2>
-      <Statistics calculatePositive={calculatePositive()} positiveText="Percentage Positive Feedback" calculateAverage={calculateAverage()} averageText="Average Feedback" badValue={bad} badText="Bad Feedback Count" neutralValue={neutral} neutralText="Neutral Feedback Count" goodValue={good} goodText="Good Feedback Count" calculateTotal={calculateTotal()} totalText="Total Feedback" allValues={good + neutral + bad}/>
+      <h1>Anecdotes</h1>
+      {anecdotes[selected]}
+      <Button handleClick={handleClick} text="Generate Anecdote" />
+      <Button handleClick={increasePoints} text="Vote"  />
+      <h4>Votes for this anecdote: {points[selected]}</h4>
+      <h3>Highest voted anecdote:</h3>
+      <div>{anecdotes[highestVoteIndex]}</div>
     </div>
   )
 }
